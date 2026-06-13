ce7d8aec09e62fa546031e39840746a438088ef9
Robin <robinz@Robins-MacBook-Pro.local>
2026-06-13 16:05:07 +0530

    My first python project

---
+++ hello.py
@@ -0,0 +1,18 @@
+robin = "hero"
+print(robin)
+my_name = "robin"
+age = 18
+height = 5.9
+is_student = True
+print("my name is",my_name)
+print("my age is",age)
+print("my height is" ,height)
+print("i am father of sethi",is_student)
+name = input("enter your name:")
+print("hello", name)
+birth_year = int(input("enter your birth year: "))
+age = 2026 - birth_year
+print("your age is", age)
+birth_year = 2008
+age = 2026 - birth_year
+print("your age is", age)
---
+++ world.py
@@ -0,0 +1,6 @@
+birth_year = int(input("Enter your birth year: "))
+age = 2026 - birth_year
+print("your age is", age)
+birth_year = 2008
+age = 2026 - birth_year
+print("your age is", age)
